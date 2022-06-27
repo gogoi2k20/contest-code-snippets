d9eaadb9b1323d9c80feec1b8011e965322b203c
gogoi2k20 <63164061+gogoi2k20@users.noreply.github.com>
2022-06-27 15:29:44 +0530

    Update binary_search_stuffs.cpp

--- binary_search_stuffs.cpp
+++ binary_search_stuffs.cpp
@@ -17,9 +17,9 @@ int startIdx(vector<int>&a,int x){
 	}
 	return ans; 
 }
-int endIdx(vector<int>&b,int x){
+int endIdx(vector<int>&a,int x){
 	int l=0;
-	int r=b.size()-1; 
+	int r=a.size()-1; 
 	int ans=-1; 
 	while(l<=r){
 		int mid=l+(r-l)/2; 
@@ -35,4 +35,22 @@ int endIdx(vector<int>&b,int x){
 		}
 	}
 	return ans; 
+}
+bool binarySearch(vector<int>&a,int x){
+	int l=0;
+	int r=a.size()-1; 
+	int ans=-1; 
+	while(l<=r){
+		int mid=l+(r-l)/2; 
+		if(a[mid]==x){
+			return true; 
+		}
+		else if(a[mid]>x){
+			r=mid-1; 
+		}
+		else {
+			l=mid+1; 
+		}
+	}
+	return false; 
 }
