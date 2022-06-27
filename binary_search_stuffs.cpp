ca6d6ec261a3ac22dede2563a50aa586725445ef
gogoi2k20 <63164061+gogoi2k20@users.noreply.github.com>
2022-06-27 15:26:26 +0530

    Update and rename start_idx_binary_search.cpp to binary_search_stuffs.cpp

--- binary_search_stuffs.cpp
+++ binary_search_stuffs.cpp
@@ -17,3 +17,22 @@ int startIdx(vector<int>&a,int x){
 	}
 	return ans; 
 }
+int endIdx(vector<int>&b,int x){
+	int l=0;
+	int r=b.size()-1; 
+	int ans=-1; 
+	while(l<=r){
+		int mid=l+(r-l)/2; 
+		if(a[mid]<x){
+			l=mid+1; 
+		}
+		else if(a[mid]==x){
+			ans=mid; 
+			l=mid+1; 
+		}
+		else {
+			r=mid-1; 
+		}
+	}
+	return ans; 
+}
