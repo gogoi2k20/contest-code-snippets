33b25a772246f577966d71c246d7ba304712b426
gogoi2k20 <63164061+gogoi2k20@users.noreply.github.com>
2022-06-27 15:24:18 +0530

    Update start_idx_binary_search.cpp

--- start_idx_binary_search.cpp
+++ start_idx_binary_search.cpp
@@ -4,16 +4,17 @@ int startIdx(vector<int>&a,int x){
 	int ans=-1;  
 	while(l<=r){
 		int mid=l+(r-l)/2; 
-		if(a[mid]>=x){
+		if(a[mid]>x){
 			ans=mid;  
 			r=mid-1; 
 		}
-		else {
-			l=mid+1;  
+		else if(a[mid]==x) {
+			ans=mid; 
+			r=mid-1; 
 		}
-	}
-	if(ans!=-1 and a[ans]!=x){
-		return -1; 
+		else {
+			l=mid+1; 
+		}
 	}
 	return ans; 
 }
